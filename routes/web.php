9564bd8fae6231128265f6a4320bd1b8adbcf358
Lori <lorenzomandolfino@gmail.com>
2024-05-21 12:01:41 +0800

    add welcome page and templates

--- routes/web.php
+++ routes/web.php
@@ -4,8 +4,12 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('pages.welcome');
+})->name('home');
+
+Route::get('welcome', function () {
+    return view('pages.welcome');
+})->name('welcome');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
